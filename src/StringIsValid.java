014a09dd8cf1061e34fbbed91adf3555b52c0c53
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-14 20:44:50 +0300

    First Commit

--- src/StringIsValid.java
+++ src/StringIsValid.java
@@ -1,17 +1,28 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class StringIsValid {
     public static void main(String[] args) {
-        isValid("))))");
-
+        boolean isValidString1 = isValid("(()()))");
+        boolean isValidString2 = isValid("(()()))");
+        boolean isValidString3 = isValid("(()()))");
+        boolean isValidString4 = isValid("(()()))");
     }
 
-    public static boolean isValid(String data){
+    public static boolean isValid(String data) {
         char[] array = data.toCharArray();
-        System.out.println("Arrays.toString(array) = " + Arrays.toString(array));
-        return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : array) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+            if (stack.peek() == '(' && c == ')')
+                stack.pop();
+            else
+                stack.push(c);
+        }
+        return stack.size()==0;
     }
-
-
 }
